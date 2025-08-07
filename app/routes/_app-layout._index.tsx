e94f5bf6257bbddae5124f8ad574c28ae0cb54d2
ray <ray472809026@hotmail.com>
2025-08-07 15:25:00 +0800

    index page redirct to first navitem page

--- app/routes/_app-layout._index.tsx
+++ app/routes/_app-layout._index.tsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { redirect } from "react-router";
+import { navItems } from "@/components/header";
+
+export async function loader() {
+  if (navItems[0]?.href) return redirect(navItems[0]?.href);
+
+  return {};
+}
 
 export default function Index() {
   return <div>Index</div>;
