12e0030cc966ec2fe7e844d51ea6af394ab4d066
ray <ray472809026@hotmail.com>
2025-08-18 10:32:54 +0800

    Add two new categoty: "Personal Care & Health" and "Mother & Baby"

---
+++ app/routes/_app-layout.personal-care-and-health.($category).tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { Route } from "./+types/_app-layout.personal-care-and-health.($category)";
+import productListLoader from "@/lib/.server/loader/product-list.loader";
+import { ProductList } from "@/components/product";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const productData = await productListLoader(request);
+  return {
+    ...productData,
+  };
+}
+
+export type Loader = typeof loader;
+
+export default function PersonalCareCategory({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      <ProductList pagination={loaderData.pagination} list={loaderData.list} />
+    </div>
+  );
+}
---
+++ app/routes/_app-layout.personal-care-and-health.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Outlet, useLoaderData, type ShouldRevalidateFunctionArgs } from "react-router";
+import type { Route } from "./+types/_app-layout.personal-care-and-health";
+import { CategoryBreadcrumb, CategoryPanel, ProductArea } from "@/components/product";
+import { categoryTreeLoader } from "@/lib/.server/loader/categoryTree.loader";
+import { shouldCategoryTreeLoaderRevalidate } from "@/lib/utils/shouldCategoryTreeLoaderRevalidate";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const categoryTree = await categoryTreeLoader(request);
+  return {
+    categoryTree,
+  };
+}
+
+export type Loader = typeof loader;
+
+export function shouldRevalidate(arg: ShouldRevalidateFunctionArgs) {
+  return shouldCategoryTreeLoaderRevalidate(arg);
+}
+
+export default function PersonalCare() {
+  const loaderData = useLoaderData<Loader>();
+
+  return (
+    <div>
+      <ProductArea
+        categoryPanel={<CategoryPanel categoryTree={loaderData.categoryTree} />}
+        categoryBreadcrumb={<CategoryBreadcrumb categoryTree={loaderData.categoryTree} />}
+        productContent={<Outlet />}
+      />
+    </div>
+  );
+}
